adaa385a180b74ab1d80e494d6a64394b6e38df9
Kimberly Lindholm <klindholm7@gmail.com>
2017-02-04 16:09:21 -0700

    implement functions -- biggest deal is load()

--- Analyst.cpp
+++ Analyst.cpp
@@ -2,24 +2,30 @@
 // Created by klind on 2/4/2017.
 //
 
+#include <fstream>
 #include "Analyst.h"
 
-void load(std::ifstream){
+void Analyst::load(std::ifstream inputStream){
+    std::string tmp;
+    std::getline(inputStream, tmp);
+    name = tmp;
+    std::getline(inputStream, tmp);
+    initials = tmp;
+    history.load(inputStream);
+}
+
+float Analyst::getStockPerformance(std::string symbol){
 
 }
 
-float getStockPerformance(std::string symbol){
-
+std::string Analyst::getName(){
+    return name;
 }
 
-std::string getName(){
-
+std::string Analyst::getInitials(){
+    return initials;
 }
 
-std::string getInitials(){
-
-}
-
-History getHistory(){
-
+History Analyst::getHistory(){
+    return history;
 }
